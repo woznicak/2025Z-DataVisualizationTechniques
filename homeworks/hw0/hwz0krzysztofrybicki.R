1ee4e2cce0cc95c352771e9441dfcd7ced4ef75e
RybKrz <krzysrybicki2005@gmail.com>
2025-10-02 19:47:12 +0200

    Add files via upload

---
+++ homeworks/hw0/hwz0krzysztofrybicki.R
@@ -0,0 +1,111 @@
+###########################################
+###    TECHNIKI WIZUALIZACJI DANYCH     ###
+###           LABORATORIUM 1            ###
+###########################################
+
+
+# 0. Prowadzący  -------------------------------------------------------------
+
+# Anna Kozak/Antoni Chudy/Iza Danielewska/Dawid Płudowski/Katarzyna Woźnica
+# Kontakt: MS Teams lub mail
+
+
+# 1. Sposób pracy na zajęciach laboratoryjnych -------------------------------
+
+# a) pracujemy w R (większość semestru) i Python
+# b) pracujemy na przygotowanych plikach, które będą na repozytorium przedmiotu
+# c) podczas zajęć prowadzący będzie wprowadzał zagdanienia, a następnie będzie rozwiązywanie zadań w celu utrwalenia wiadomości
+# d) kolejna porcja materiału będzie omawiana jeżeli większość grupy wykona zadane zadanie 
+# e) wszelkie pytania czy to związane z kodem, pracą domową czy kwestie teoretyczne proszę śmiało zgłaszać prowadzącemu 
+
+
+# 2. Materiały ------------------------------------------------------------
+
+# Repozytorium na GitHub
+# https://github.com/kozaka93/2025Z-DataVisualizationTechniques
+
+
+# 3. Jak działa GitHub? ---------------------------------------------------
+
+# Jak zgłosić pracę domową/projekt? (fork, commit, pull request)
+# https://rogerdudler.github.io/git-guide/
+
+
+# 4. Podstawy R - rozgrzewka ----------------------------------------------
+
+data(mtcars)
+head(mtcars)
+
+?mtcars
+
+
+# Jak wybieramy wiersze (obserwacje) oraz kolumny (zmienne)?
+
+
+# Pierwszy wiersz, pierwsza kolumna?
+
+
+# 10 pierszych wierszy, 2 i 3 kolumna?
+
+
+# Jak wybieramy kolumny po nazwach? 
+
+
+# Wszystkie wiersze i kolumny w kolejności "am", "wt", "mpg"?
+
+
+# Jak wybierać jedną kolumnę?
+
+# Uwaga na przecinek i wybór kolumn poprzez indeksy
+
+# Pytania
+
+# 1. Wymiar ramki danych
+
+
+# 2. Jakie są typy zmiennych?
+
+
+# 3. Ile jest unikalnych wartości zmiennej "cyl" i jakie to są wartości?
+
+
+# 4. Jaka jest średnia wartość zmiennej "drat" dla samochodów 
+# o wartości zmiennej "cyl" równej 4?
+
+
+# 5. Jakie są unikalne wartości zmiennej "am" i jaki jest ich rozkład (liczba wystąpień)? 
+
+
+
+# Prosty wykres
+
+
+# Zależność "mpg" i "hp" - scatter plot
+
+
+# Zmienna "cyl" - barplot
+
+
+# 5. Zadanie "proton" -----------------------------------------------------
+
+# Utwórz nowy plik .R. Zainstaluj/uruchom pakiet proton:
+install.packages("proton")
+library(proton)
+employees[employees$surname=="Insecure",]
+proton(action = "login", login="johnins")
+vec=top1000passwords
+for (el in vec) { if (proton(action = "login", login="johnins", password=el)=="Success! User is logged in!") {print(el)}}
+employees[employees$surname=="Pietraszko",]
+logs[logs$login=="slap",]
+adr=logs[logs$login=="slap",c("host")]
+which.max(table(adr))
+proton(action = "server", host="194.29.178.16")
+his=bash_history
+for (el in his) { if (proton(action = "login", login="slap", password=el)=="Success! User is logged in!") {print(el)}}
+"DHbb7QXppuHnaXGN"
+# W pliku zapisuj sposób rozwiązania gry.
+
+
+
+
+# 6. Zamieszczamy rozwiązanie na repozytorium -----------------------------
