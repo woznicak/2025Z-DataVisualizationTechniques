8173a43ce3e55e089793a727ae714b64b8526081
Alexandra <105586261+mulewicz@users.noreply.github.com>
2025-10-02 21:28:05 +0200

    hw0 test

---
+++ homeworks/hw0/MulewiczAleksandra/mulewicz.R
@@ -0,0 +1,18 @@
+install.packages("proton")
+library(proton)
+
+#employees[c(employees$name == "John", employees$surname == "Insecure"), "login"]
+#proton(action = "login", login="johnins")
+
+#responses <- sapply(top1000passwords, function(x) proton(action = "login", login="johnins", password=x))
+#which(responses == "Success! User is logged in!
+#proton(action = "login", login = "johnins", password = "qiw2e3r4t5)
+
+#employees[employees$surname == "Pietraszko", "login"]
+#host_num <- table(pietraszko_logs$host)
+#most <- names(which.max(host_num))
+#proton(action = "server", host = most)
+#"194.29.178.16"
+
+#table(sapply(strsplit(bash_history, " "), `[`, 1))
+#DHbb7QXppuHnaXGN 
