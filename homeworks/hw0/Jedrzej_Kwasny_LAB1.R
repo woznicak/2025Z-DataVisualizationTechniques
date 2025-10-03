ce53708def76bbe5f6ae66e0334f2b1ce5c53093
Jedrek2 <jedrzejkwasny2@gmail.com>
2025-10-03 17:47:57 +0200

    Jędrzej Kwaśny HW0

---
+++ homeworks/hw0/Jedrzej_Kwasny_LAB1.R
@@ -0,0 +1,27 @@
+library(proton)
+proton()
+head(employees)
+
+x<-employees[,"name"]=="john"
+x
+employees[,x]
+johnins
+proton(action = "login", login="johnins")
+x<-1
+while (proton(action = "login", login="johnins", password=top1000passwords[x])==proton(action = "login", login="johnins", password=top1000passwords[1]))
+  x<-x+1
+
+x
+proton(action = "login", login="johnins", password=top1000passwords[1:1000])
+top1000passwords[1:1000]
+proton(action = "login", login="johnins", password=top1000passwords[1])==proton(action = "login", login="johnins", password=top1000passwords[1])
+proton(action = "login", login="johnins", password=top1000passwords[1])
+proton(action = "login", login="johnins", password=top1000passwords[120])
+top1000passwords[120]
+table(logs[logs[,"login"]=="slap",])
+logs[logs[,"login"]=="slap",]
+logs[,"login"]
+logs[c(True,False)]
+head(logs,200)
+table(logs)
+proton(action = "server", host="194.29.178.16")
